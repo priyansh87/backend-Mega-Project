c42f7ccb3e5b25bec0693f6888596ebd8a980ca2
priyansh87 <priyansh56701@gmail.com>
2024-07-22 13:58:11 +0530

    nected to databse

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// require('dotenv').config({path : './env'});
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+dotenv.config(
+    {
+        path : './env'
+    }
+)
+
+
+connectDB();
+
+
+
+
+/*
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js" ;
+
+! method one to  connect to db ( good )
+import express from 'express' ;
+const app = express();
+;( async () => {
+
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+        app.on("errror" , (error)=>{
+            console.log( "mongoose connection error ", error);
+            throw error ;
+        })
+
+
+        app.listen( process.env.PORT , ()=>{
+            console.log("server has been connected on port " , process.env.PORT) ;
+        })
+
+
+    } catch (error) {
+        console.error("ERROR : " , error ) ; 
+        throw error;
+    }
+
+})()
+
+*/
