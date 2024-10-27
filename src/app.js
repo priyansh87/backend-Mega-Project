28158efbb0817ead072909a2cd44e9a0018f5fe8
priyansh87 <priyansh56701@gmail.com>
2024-10-27 21:29:12 +0530

    user controller made

--- src/app.js
+++ src/app.js
@@ -15,5 +15,17 @@ app.use( express.static("public"))
 app.use( cookieParser() )
 
 
+// routes : 
+import userRouter from "./routes/user.routes.js" 
+
+
+//routes declaration 
+// ! jb koi /user pe ayega tb control userRouter pe jayega aur vo user.routes.js mai jayega and vaha kaam krega 
+// ! user ke baad ke jitne bhi routes hai vo sb ab userRutes main hai jaise ki 
+// ! http://localhost:9000/api/v1/users/register -> register router file mai hai  aur register jo krega vo controller mai hai 
+// ! http://localhost:9000/api/v1/users/login -> login router file mai hai  aur login jo krega vo controller mai hai 
+app.use("/api/v1/users", userRouter )  // user routes are mounted here
+
+
 
 export  { app }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema ( {
 userSchema.pre ( "save" , async function(next){
     // if the password in the current model is not modified then just skip encrypting 
     if( !this.isModified("password")) return next();
-    this.password = bcrypt.hash( this.password , 10)
+    this.password = await bcrypt.hash( this.password , 10)
     next()
 })
 // methods in mongoose 
